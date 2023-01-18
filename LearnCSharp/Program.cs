02fc66bbff1ad09570c98d4c5be7e507ea95e6e4
Jonas Diete <jonas.diete@gmail.com>
2023-01-18 13:20:40 +0000

    Add project files.

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome!");
+        }
+    }
+}
