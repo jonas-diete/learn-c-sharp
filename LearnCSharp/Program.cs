d14f9882a8c198783a289443b1f137d93334a089
Jonas Diete <jonas.diete@gmail.com>
2023-01-18 14:16:03 +0000

    testing maths

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -6,11 +6,16 @@ namespace LearnCSharp
     {
         static void Main(string[] args)
         {
+            // testing variables
             Console.WriteLine("Hello and welcome!");
             int age = 21;
             const string MonthOfBirth = "October";
             Console.WriteLine("You are " + age + " years old.");
             Console.WriteLine("You were born in " + MonthOfBirth + ".");
+
+            // Testing some maths
+            Console.WriteLine("5 / 2 = " + 5 / 2);
+            Console.WriteLine("5.0 / 2 = " + 5.0 / 2);
         }
     }
 }
