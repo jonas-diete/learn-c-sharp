644dc3c5971e99f4070e76e0ea43547a71c02e10
Jonas Diete <jonas.diete@gmail.com>
2023-01-18 14:18:52 +0000

    playing with multiple variables

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -16,6 +16,11 @@ namespace LearnCSharp
             // Testing some maths
             Console.WriteLine("5 / 2 = " + 5 / 2);
             Console.WriteLine("5.0 / 2 = " + 5.0 / 2);
+
+            // multiple variables
+            int x, y, z;
+            x = y = z = 50;
+            Console.WriteLine(x + y + z);
         }
     }
 }
