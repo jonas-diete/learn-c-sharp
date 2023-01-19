82f862a09149f7c9813afec435a5f691f6300244
Jonas Diete <jonas.diete@gmail.com>
2023-01-19 08:44:52 +0000

    practised string functions

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -22,16 +22,27 @@ namespace LearnCSharp
             x = y = z = 50;
             Console.WriteLine(x + y + z);
 
-            // User input
+            // arithmetic and logical operators are the same as in JavaScript, which I know already.
+
+            // user input
             Console.WriteLine("Please enter your height in cm: ");
             int heightInCm = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your height is " + heightInCm + "cm.");
             heightInCm++;
             Console.WriteLine("It appears you have grown by 1cm. Your new height is " + heightInCm + "cm.");
+
+            // math functions
             int largestHeight = Math.Max(141, 125);
             Console.WriteLine(largestHeight + "cm is the largest height.");
             int shortestHeight = Math.Min(141, 125);
             Console.WriteLine(shortestHeight + "cm is the shortest height.");
+
+            // string functions
+            Console.Write("Write a greeting: ");
+            string greeting = Console.ReadLine();
+            Console.WriteLine("Your greeting is " + greeting.Length + " characters long.");
+            Console.WriteLine("Uppercase: " + greeting.ToUpper());
+            Console.WriteLine("Lowercase: " + greeting.ToLower());
         }
     }
 }
