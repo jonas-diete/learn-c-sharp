fded1cee813540f24578bf471a15c742bd9b253c
Jonas Diete <jonas.diete@gmail.com>
2023-01-19 08:20:09 +0000

    added user input

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -9,18 +9,23 @@ namespace LearnCSharp
             // testing variables
             Console.WriteLine("Hello and welcome!");
             int age = 21;
-            const string MonthOfBirth = "October";
+            const string monthOfBirth = "October";
             Console.WriteLine("You are " + age + " years old.");
-            Console.WriteLine("You were born in " + MonthOfBirth + ".");
+            Console.WriteLine("You were born in " + monthOfBirth + ".");
 
             // Testing some maths
             Console.WriteLine("5 / 2 = " + 5 / 2);
-            Console.WriteLine("5.0 / 2 = " + 5.0 / 2);
+            Console.WriteLine("5.0 / 2 = " + 5.0D / 2);
 
             // multiple variables
             int x, y, z;
             x = y = z = 50;
             Console.WriteLine(x + y + z);
+
+            // User input
+            Console.WriteLine("Please enter your height in cm: ");
+            int heightInCm = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your height is " + heightInCm + "cm.");
         }
     }
 }
